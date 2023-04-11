6fb6282a49e0d657ae94fc13bdf231cd2b425be1
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-04-11 13:18:56 +0200

    feat: implement task-4 according to module 4
    - implement module 4

--- src/tests/components/pages/HomePage.test.tsx
+++ src/tests/components/pages/HomePage.test.tsx
@@ -10,37 +10,4 @@ describe('HomePage.tsx', () => {
         render(<HomePage />);
         expect(screen.queryByTestId('pageTitle')).toBeInTheDocument();
     });
-
-    test('fetches data and renders correctly', async () => {
-        // Mock Axios response
-        const mockedResponse = {
-            data: {
-                created: "2017-11-04T18:50:21.651Z",
-                episode: ['episode1'],
-                gender: 'Male',
-                id: 23403043,
-                image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-                name: "Test Name",
-                location: {
-                    name: "Test Name",
-                    url: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-                },
-                origin: {
-                    name: "Test Name",
-                    url: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-                },
-                species: "Human",
-                status: "Alive",
-                type: "",
-                url: "string",
-            }
-        };
-        (axios.get as jest.Mock).mockReturnValue(mockedResponse);
-
-        render(<HomePage />)
-
-        await waitFor(() => screen.getByText('Test Name'));
-        expect(screen.getByText('Test Name')).toBeInTheDocument();
-        expect(screen.getByText('Alive')).toBeInTheDocument();
-    });
 });
